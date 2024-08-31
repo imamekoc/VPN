767be54a42ab7e9c77abfc1a48e904fdaa9e7808
Tino Imam M P <tinoimammp@gmail.com>
2024-09-01 03:01:31 +0700

    rm -y

--- setupku.sh
+++ setupku.sh
@@ -28,8 +28,8 @@ ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
 
-apt install git curl -y >/dev/null 2>&1
-apt install python -y >/dev/null 2>&1
+apt install git curl >/dev/null 2>&1
+apt install python >/dev/null 2>&1
 echo -e "[ ${green}INFO${NC} ] Aight good ... installation file is ready"
 sleep 2
 
